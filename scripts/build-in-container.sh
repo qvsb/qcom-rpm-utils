6979066056f56730fe1091e5103c8ab9451fc471
qvsb <bathina@qti.qualcomm.com>
2026-09-16 16:45:45 +0530

    ci: increase priority of extra repo
    Increase priority of extra 
    repo to prioritise qcom
    version over latest version of that package
    
    
    Signed-off-by: qvsb <bathina@qti.qualcomm.com>

--- scripts/build-in-container.sh
+++ scripts/build-in-container.sh
@@ -68,7 +68,7 @@ rpmspec -P "/root/rpmbuild/SPECS/${spec_base}" \
 
 if [[ -n "${EXTRA_REPO_DIR}" ]]; then
     echo "Registering extra dnf repo: ${EXTRA_REPO_DIR}"
-    printf '[extra-repo]\nname=Extra RPM Repository\nbaseurl=%s\nenabled=1\ngpgcheck=0\nskip_if_unavailable=1\n' \
+    printf '[extra-repo]\nname=Extra RPM Repository\nbaseurl=%s\nenabled=1\ngpgcheck=0\nskip_if_unavailable=1\npriority=1\n' \
         "${EXTRA_REPO_DIR}" > /etc/yum.repos.d/extra-repo.repo
     dnf clean metadata
 fi
